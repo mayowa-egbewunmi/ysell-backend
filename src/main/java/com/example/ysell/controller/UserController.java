3a1da27e2066a9d42eaea8b15eabc861cd59356e
Mayowa Egbewunmi <mayowaegbewunmi@gmail.com>
2020-06-29 17:22:15 +0100

    Database migration sample

--- src/main/java/com/example/ysell/controller/UserController.java
+++ src/main/java/com/example/ysell/controller/UserController.java
@@ -1,20 +1,26 @@
 package com.example.ysell.controller;
 
+import com.example.ysell.Entities.UserEntity;
 import com.example.ysell.config.UserClient;
 import com.example.ysell.model.Meta;
 import com.example.ysell.model.Response;
 import com.example.ysell.model.User;
+import com.example.ysell.service.UserService;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/mobile")
 public class UserController {
+	
     private final UserClient userClient;
+    private final UserService userService;
 
-    public UserController( UserClient userClient) {
+    public UserController( UserClient userClient, UserService userService) {
 
         this.userClient = userClient;
+        this.userService = userService;
     }
 
     @PostMapping("/account/create")
@@ -38,5 +44,13 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public Response<Meta, User> update(){
         return userClient.updateUser();
+    }
+    
+    @GetMapping("/user/get/{id}")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public UserEntity getUser(@PathVariable(value = "id") Long id) {
+    	UserEntity user = userService.getUserByEmail(id);
+    	return user;
     }
 }
--- src/main/java/com/example/ysell/service/UserService.java
+++ src/main/java/com/example/ysell/service/UserService.java
@@ -3,20 +3,24 @@ package com.example.ysell.service;
 import com.example.ysell.Entities.UserEntity;
 import com.example.ysell.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class UserService {
-
+	
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+	
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
     public UserEntity createUser(final UserEntity userEntity) {
-
         return userRepository.saveAndFlush(userEntity);
-
+    }
+    
+    public UserEntity getUserByEmail(final Long id) {
+    	UserEntity user = new UserEntity();
+    	user.setEmail("mayowaegbewunmi@gmail.com");
+    	return userRepository.findById(id).orElse(user);
     }
 }
